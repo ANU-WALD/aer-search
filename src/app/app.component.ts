2151d8b65808071d458a01163a812d70e8592700
Joel Rahman <joel@flowmatters.com.au>
2020-03-24 15:41:32 +1100

    Switch to use typeahead for lookup of reports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { GeocoderService } from './geocoder.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, of } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged, switchAll, publishReplay, refCount } from 'rxjs/operators';
 
 const DEBOUNCE_TIME=50;
 
@@ -39,6 +40,7 @@ interface Report {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  layers$: Observable<LayerDescription[]>;
   layers: LayerDescription[] = [];
   selectedLayer: LayerDescription;
   selectedFeature: SimpleFeature;
@@ -49,10 +51,16 @@ export class AppComponent {
   location:Location = null;
   matchedAddress: string;
   matchedReports:Report[];
-  selectedReport: Report;
+  selectedReport: string | Report;
 
   constructor(private _http: HttpClient, private geocoder:GeocoderService) {
-    this._http.get(`${environment.config}?_t=${(new Date().getTime())}`).subscribe((data: LayerDescription[]) => {
+    const url = `${environment.config}?_t=${(new Date().getTime())}`;
+    this.layers$ = this._http.get(url).pipe(
+      map((d:LayerDescription[])=>d),
+      publishReplay(),
+      refCount());
+
+    this.layers$.subscribe((data: LayerDescription[]) => {
       this.layers = data;
       this.selectedLayer = data[0];
       this.layerChange();
@@ -68,9 +76,9 @@ export class AppComponent {
     this.updateURL();
   }
 
-  featureChange() {
-    this.updateURL();
-  }
+  // featureChange() {
+  //   this.updateURL();
+  // }
 
   addressChange() {
     if(this.debounce!==null){
@@ -78,6 +86,7 @@ export class AppComponent {
       this.debounce = null;
     }
 
+    this.matchedAddress = null;
     this.debounce = window.setTimeout(()=>{
       if(this.geocodingSubscription){
         this.geocodingSubscription.unsubscribe();
@@ -101,10 +110,7 @@ export class AppComponent {
   }
 
   updateURL() {
-    this.selectedReport = {
-      label: `${this.selectedLayer.name}: ${this.selectedFeature[this.selectedLayer.labelField]}`,
-      url: this.makeURL(this.selectedLayer,this.selectedFeature)
-    }
+    this.selectedReport = this.makeReport(this.selectedLayer,this.selectedFeature);
   }
 
   makeURL(layer:LayerDescription,feature:SimpleFeature):string{
@@ -112,14 +118,25 @@ export class AppComponent {
     return `${base}/${layer.source}/${feature[layer.labelField]}.pdf`;
   }
 
+  makeReport(lyr:LayerDescription,feature:SimpleFeature):Report{
+    return {
+      label: `${feature[lyr.labelField]} (${lyr.name})`,
+      url: this.makeURL(lyr,feature)
+    };
+  }
+
   useLocation() {
+    this.address = null;
+    this.matchedAddress = null;
+    this.selectedReport = null;
     navigator.geolocation.getCurrentPosition(pos=>{
-      console.log(pos);
       const coords = pos&&pos.coords;
       if(!coords){
         return;
       }
-      this.matchedAddress = `${coords.latitude}, ${coords.longitude}`;
+      this.matchedAddress =
+        `${coords.latitude.toFixed(4)}, ${coords.longitude.toFixed(4)}`;
+
       this.location = {
         lat:coords.latitude,
         lng:coords.longitude
@@ -137,13 +154,35 @@ export class AppComponent {
     this.matchedReports = [];
     this.layers.forEach(layer=>{
       const matchingFeatures = layer.features.filter(f=>this.featureContains(f,this.location));
-      this.matchedReports = this.matchedReports.concat(matchingFeatures.map(f=>{
-        return {
-          label: `${layer.name}: ${f[layer.labelField]}`,
-          url: this.makeURL(layer,f)
-        };
-      }));
+      this.matchedReports = this.matchedReports.concat(
+        matchingFeatures.map(f=>this.makeReport(layer,f)));
     });
+  }
+
+  matchReportsByLocation(loc:Location):Observable<Report[]>{
+    return this.layers$.pipe(
+      map(layers=>{
+        return layers.map(lyr=>{
+          return lyr.features.filter(f=>{
+            return this.featureContains(f,this.location);
+          }).map(f=>this.makeReport(lyr,f))
+        });
+      }),
+      map(reportsByLayer=>([] as Report[]).concat(...reportsByLayer)));
+  }
+
+  matchReportsByName(txt:string):Observable<Report[]>{
+    txt = txt.toLowerCase();
+    return this.layers$.pipe(
+      map(layers=>{
+        return layers.map(lyr=>{
+          return lyr.features.filter(f=>{
+            const lbl = f[lyr.labelField]&&f[lyr.labelField].toString().toLowerCase();
+            return lbl.indexOf(txt)>=0;
+          }).map(f=>this.makeReport(lyr,f));
+        });
+      }),
+      map(reportsByLayer=>([] as Report[]).concat(...reportsByLayer)));
   }
 
   private featureContains(f:SimpleFeature,location:Location):boolean{
@@ -151,5 +190,33 @@ export class AppComponent {
            location.lat <= f.north &&
            location.lng >= f.west &&
            location.lng <= f.east;
+  }
+
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => {
+        if(term.length < 2){
+          return of([]);
+        }
+        let matches$ = this.matchReportsByName(term);
+        if(term.length < 4){
+          return matches$;
+        }
+
+        const geocoding$ = this.geocoder.getCoordinates(this.address).pipe(
+          map(loc=>loc[0])
+        );
+
+        return matches$;
+      }),
+      switchAll()
+    );
+
+  reportFormatter = (r:Report) => r.label;
+
+  onNavChange(){
+    this.selectedReport = null;
   }
 }
