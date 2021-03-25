0b3278b0bd2f4dfb2951dd6e8498a5435a41cca1
Joel Rahman <joel@flowmatters.com.au>
2021-03-25 17:24:25 +1100

    Revert PDF location

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // pdf_location:'http://wenfo.org/aer_pdf',
-  pdf_location:'https://www.flowmatters.com.au/aer_pdf',
+  pdf_location:'http://wenfo.org/aer_pdf',
   config:'assets/report_card_search.json',
   google_maps_api_key: 'WENFO_GOOGLE_MAPS_API_KEY'
 };
