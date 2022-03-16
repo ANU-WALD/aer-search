d437d7f19535d18c2caac172a1f40217a171e382
Joel Rahman <joel@flowmatters.com.au>
2022-03-16 13:44:45 +1100

    Update PDF path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  pdf_location:'http://wenfo.org/aer_pdf',
+  pdf_location:'http://wenfo.org/aer_pdf_2021',
   config:'assets/report_card_search.json',
   google_maps_api_key: 'WENFO_GOOGLE_MAPS_API_KEY'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  pdf_location:'http://wenfo.org/aer_pdf',
+  pdf_location:'http://wenfo.org/aer_pdf_2021',
   config:'assets/report_card_search.json',
   google_maps_api_key: 'WENFO_GOOGLE_MAPS_API_KEY'
 };
